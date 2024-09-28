3f9957fa281e7999b2570e138f691b344895ce10
Ayoub-EL-ATMANI <ayoubelatmani50@gmail.com>
2024-09-29 00:21:54 +0100

    project java spring and kafka for test the difference type strategies for produce mevent to kafka

---
+++ src/test/java/com/app/kf/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
